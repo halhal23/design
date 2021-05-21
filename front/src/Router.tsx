7e2cde71589934dcd9f5494b36e37d50e9ff8456
halhal <hh.25799623@gmail.com>
2021-05-21 16:17:19 +0900

    install react-parallax-tilt

--- front/src/Router.tsx
+++ front/src/Router.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home'
+import ParallaxTilt from './pages/ParallaxTilt'
 import Test from './pages/Test'
 
 const Router: React.FC = () => {
@@ -9,6 +10,7 @@ const Router: React.FC = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/test" component={Test} />
+        <Route exact path="/parallaxTilt" component={ParallaxTilt} />
       </Switch>
     </BrowserRouter>
   )
---
+++ front/src/pages/ParallaxTilt.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ParallaxTilt: React.FC = () => {
+  return <div>parallax</div>
+}
+
+export default ParallaxTilt
